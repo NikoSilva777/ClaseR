b83d4d54a8a53b58a6a672d5248f99ef6655cf42
NikoSilva777 <nikosilvabike@gmail.com>
2025-07-10 19:46:05 -0400

    Cargas

---
+++ ConeccionGitHub.R
@@ -0,0 +1,9 @@
+install.packages(c("usethis", "renv", "gh"))
+
+usethis::use_git()
+
+usethis::use_github()
+
+usethis::create_github_token()
+
+usethis::edit_r_environ()
---
+++ GitHub.R
@@ -0,0 +1,8 @@
+install.packages(c("usethis", "renv", "gh"))
+
+usethis::use_git()
+
+usethis::use_github()
+
+usethis::create_github_token()
+usethis::edit_r_environ()
